682fde95c43ff3acceaeefb4ba49f51a8e67da79
Thalya Stéffany <87552041+thalya-codes@users.noreply.github.com>
2024-04-22 23:16:57 -0300

    feat: implementa estrutura de dados pilha

---
+++ stack/implementation.js
@@ -0,0 +1,45 @@
+class Stack {
+ stack = [];
+
+ constructor(limit) {
+  this.limit = limit;
+ }
+
+ push(value) {
+  const isFull = this.isFull();
+
+  if (!isFull) return this.stack.push(value);
+
+  console.log({ isFull });
+ }
+
+ pop() {
+  const isEmpty = isEmpty();
+
+  if (!isEmpty) return this.pilha.pop();
+
+  console.log(this.isEmpty());
+ }
+
+ peek() {
+  return this.stack.findLast((data) => data);
+ }
+
+ isEmpty() {
+  return this.stack.length === 0;
+ }
+
+ isFull() {
+  return this.stack.length === this.limit;
+ }
+}
+
+const numbersStack = new Stack(5);
+numbersStack.push(10);
+numbersStack.push(20);
+numbersStack.push(300);
+numbersStack.push(500);
+numbersStack.push(70);
+numbersStack.push(170);
+
+console.log(numbersStack.peek(), numbersStack);
